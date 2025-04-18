9154f5b673176741ee31ee9e56554191aa3223f3
HexLab-NUC12-MasterYip <2205929492@qq.com>
2025-04-18 22:57:22 +0800

    bash_launch

--- hexapod_elevation_mapping/scripts/brige.sh
+++ hexapod_elevation_mapping/scripts/brige.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 自动选择 ROS 2
-echo 2 | source /opt/ros/galactic/setup.bash
+source /opt/ros/galactic/setup.bash
 export ROS_DOMAIN_ID=42
-source /opt/ros/noetic/setup.bash
+#source /opt/ros/noetic/setup.bash
 ros2 run ros1_bridge dynamic_bridge --bridge-all-topics
